0fbcf8502515c32fbec3edab75c555ac2177b22a
mariomadproductions <mariomadproductions@users.noreply.github.com>
2020-12-13 16:18:03 +0000

    new file:   txt2dat.py

---
+++ txt2dat.py
@@ -0,0 +1,97 @@
+from sys import argv, stdin
+import pyperclip
+import detect_delimiter
+from xml.etree import ElementTree
+from xml.etree.ElementTree import Element
+from xml.etree.ElementTree import SubElement
+
+FIELD_MODE_TESTS = {'field_mode__size' : \
+                    [['size', 'size in bytes'],'size'],
+                    
+                    'field_mode__crc32' : \
+                    [['crc', 'crc32', 'crc-32'],'crc'],
+                    
+                    'field_mode__md5' : \
+                    [['md5', 'md-5', 'md5sum'],'md5'],
+                    
+                    'field_mode__sha1' : \
+                    [['sha1', 'sha-1', 'sha1sum'],'sha1'],
+                    
+                    'field_mode__sha256' : \
+                    [['sha256', 'sha-256', 'sha256sum'],'sha256']}
+
+def dict_to_xml(mydict):
+    game = Element('game')
+    subelement = SubElement(game, 'rom')
+    for key, value in mydict.items():
+        subelement.set(key, value)
+    return ElementTree.tostring(game,encoding='unicode')
+
+def test_field_mode(field_mode_label,key):
+    key = key.lower()
+    for string in FIELD_MODE_TESTS[field_mode_label][0]:
+        if string == key:
+            return field_mode_label, FIELD_MODE_TESTS[field_mode_label][1]
+    return None, None
+
+def text_to_dict(text):
+    mydict = {}
+    delim = detect_delimiter.detect(text[0], whitelist=[':',' '])
+    if delim == None:
+        raise ValueError('Cannot detect delimiter.')
+    for line in text:
+        if line.strip() != '':
+            line_split = line.split(sep=delim, maxsplit=1)
+            key = line_split[0].strip()
+            value = line_split[1].strip()
+
+            field_mode = 'field_mode__size'
+            field_test_results = test_field_mode(field_mode,key)
+            if field_test_results[0] == field_mode:
+                key = field_test_results[1]
+                value = value.replace(' ','') \
+                             .replace(',','') \
+                             .replace('bytes','')
+
+            field_mode = 'field_mode__crc32'
+            field_test_results = test_field_mode(field_mode,key)
+            if field_test_results[0] == field_mode:
+                key = field_test_results[1]
+                value = value.upper().replace(' ','')
+
+            field_mode = 'field_mode__md5'
+            field_test_results = test_field_mode(field_mode,key)
+            if field_test_results[0] == field_mode:
+                key = field_test_results[1]
+                value = value.upper().replace(' ','')
+
+            field_mode = 'field_mode__sha1'
+            field_test_results = test_field_mode(field_mode,key)
+            if field_test_results[0] == field_mode:
+                key = field_test_results[1]
+                value = value.upper().replace(' ','')
+
+            field_mode = 'field_mode__sha256'
+            field_test_results = test_field_mode(field_mode,key)
+            if field_test_results[0] == field_mode:
+                key = field_test_results[1]
+                value = value.upper().replace(' ','')
+            
+            mydict[key] = value
+    return mydict
+
+def main(text):
+    CORRECT_FIELD_ORDER = ['size', 'crc', 'md5', 'sha1', 'sha256']
+    text_split = text.splitlines()
+    mydict = text_to_dict(text_split)
+    for key in CORRECT_FIELD_ORDER:
+        try:
+            mydict[key] = mydict.pop(key)
+        except KeyError:
+            pass
+    xml = dict_to_xml(mydict)
+    return(xml)
+    
+if __name__ == '__main__':
+    text = pyperclip.paste()
+    pyperclip.copy(main(text))
